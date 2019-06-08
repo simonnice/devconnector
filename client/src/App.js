3931073435f1936ac46d39c6c7dc795d10f64911
sb22ix <sb22ix@student.lnu.se>
2019-06-08 07:50:06 +0200

    Prepared for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import Post from "./components/post/Post";
 
 // Redux
 import { Provider } from "react-redux";
@@ -69,6 +70,7 @@ const App = () => {
               />
 
               <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
           </section>
         </Fragment>
